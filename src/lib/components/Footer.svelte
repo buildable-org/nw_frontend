b69dbf5e46e488e44661e74534dfe1e0be1a507e
Ripardo <roope.ripatti@gmail.com>
2025-01-08 13:14:27 +0200

    Changed the mobile footer to be less cluttered

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,55 +1,67 @@
 <script>
-    // You can add any necessary script here
+  // You can add any necessary script here
 </script>
 
-<style>
-    footer {
-        display: flex;
-        justify-content: space-between;
-        padding: 2rem 5rem 2rem 5rem;
-        background: none;
-        color: white;
-    }
-
-    .nav-links, .social-links {
-        display: flex;
-        gap: 1rem;
-    }
-
-    a {
-        color: white;
-        text-decoration: none;
-    }
-
-    a:hover {
-        text-decoration: underline;
-    }
-
-    .social-links img {
-        width: 32;
-        height: 32px;
-        filter: invert(1);
-    }
-
-    @media (max-width: 768px) {
-        footer {
-            padding: 2rem 2rem 2rem 2rem;
-        }
-    }
-</style>
-
 <footer>
-    <div class="nav-links">
-        <a href="/">Home</a>
-        <a href="/faq">FAQ</a>
-        <a href="/apply">Application</a>
-    </div>
-    <div class="social-links">
-        <a href="https://x.com/_buildable_" target="_blank" rel="noopener noreferrer">
-            <img src="/x.svg" alt="X social media">
-        </a>
-        <a href="https://www.linkedin.com/company/buildable-school" target="_blank" rel="noopener noreferrer">
-            <img src="/linkedin.svg" alt="LinkedIn">
-        </a>
-    </div>
+  <div class="nav-links">
+    <a href="/">Home</a>
+    <a href="/faq">FAQ</a>
+    <a href="/apply">Application</a>
+  </div>
+  <div class="social-links">
+    <a
+      href="https://x.com/_buildable_"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src="/x.svg" alt="X social media" />
+    </a>
+    <a
+      href="https://www.linkedin.com/company/buildable-school"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src="/linkedin.svg" alt="LinkedIn" />
+    </a>
+  </div>
 </footer>
+
+<style>
+  footer {
+    display: flex;
+    justify-content: space-between;
+    padding: 2rem 5rem 2rem 5rem;
+    background: none;
+    color: white;
+  }
+
+  .nav-links,
+  .social-links {
+    display: flex;
+    gap: 1rem;
+  }
+
+  a {
+    color: white;
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  .social-links img {
+    width: 32;
+    height: 32px;
+    filter: invert(1);
+  }
+
+  @media (max-width: 768px) {
+    footer {
+      padding: 2rem 2rem 2rem 2rem;
+      flex-direction: column;
+      align-items: center;
+      gap: 1rem;
+    }
+  }
+</style>
